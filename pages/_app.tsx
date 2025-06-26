8641da3c6f7f735ed466c012ac8d69f1d86744e3
jeanscroll <jean@agence-scroll.com>
2025-06-26 11:58:58 +0200

    Install posthog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,29 @@
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import { Router } from 'next/router';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
 import { LocaleProvider } from '@/contexts/LocaleContext';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host: '/ingest',
+      ui_host: 'https://eu.posthog.com',
+      capture_pageview: 'history_change',
+      capture_exceptions: true, // This enables capturing exceptions using Error Tracking
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === 'development') posthog.debug();
+      },
+      debug: process.env.NODE_ENV === 'development',
+    });
+  }, []);
+
   return (
-    <LocaleProvider>
-      <Component {...pageProps} />
-    </LocaleProvider>
+    <PostHogProvider client={posthog}>
+      <LocaleProvider>
+        <Component {...pageProps} />
+      </LocaleProvider>
+    </PostHogProvider>
   );
-} 
+}
