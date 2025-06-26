5d8e0c16000a00ac2135598322d5a0c62a3cb1a0
jeanscroll <jean@agence-scroll.com>
2025-06-26 17:38:54 +0200

    fix again posthog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app';
 import { useEffect } from 'react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Router } from 'next/router';
 import posthog from 'posthog-js';
 import { PostHogProvider } from 'posthog-js/react';
 import { LocaleProvider } from '@/contexts/LocaleContext';
@@ -11,7 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
       api_host: '/ingest',
       ui_host: 'https://eu.posthog.com',
-      capture_pageview: 'history_change',
+      defaults: '2025-05-24', // This enables capture_pageview: 'history_change' by default
       capture_exceptions: true, // This enables capturing exceptions using Error Tracking
       loaded: (posthog) => {
         if (process.env.NODE_ENV === 'development') posthog.debug();
