da7a4586bc1f2d706056b0e87e1964c741d0fe1b
jeanscroll <jean@agence-scroll.com>
2025-07-08 13:08:10 +0200

    fix

--- android/app/src/main/java/com/sifem/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.sifem.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.sifem.app',
+  appId: 'org.sifem.dpei-pocket',
   appName: 'Sifem',
   webDir: 'out'
 };
