83c8b62f444f874b5475172e33fd32204b8986e7
jeanscroll <jean@agence-scroll.com>
2025-07-21 09:53:33 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: 'web' pour webapp, output: 'export' pour exportation
+  // output: 'web' pour webapp, output: 'export' pour exportation - push
   output: 'export',
   async rewrites() {
     return [
