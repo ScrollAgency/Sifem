76c3ad57d7050c730f5924556d2269cc5b9f3907
jeanscroll <jean@agence-scroll.com>
2025-07-22 10:11:11 +0200

    Release

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'org.sifem.dpei-pocket',
+  appId: 'org.sifem.dpei_pocket',
   appName: 'dPEI Pocket',
   webDir: 'out'
 };
