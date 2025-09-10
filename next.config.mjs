42d04034ead6b4054a8cf2265f182e230175af2a
Gael <prigentgael@gmail.com>
2025-09-10 17:04:31 +0200

    remove double images

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const baseConfig = {
-  //output: 'export',
+  output: 'export',
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
